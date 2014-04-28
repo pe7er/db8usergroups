3478ca72107d267f3dc2e2fb4d94fd9482a75d35
Peter Martin <pe7er@thinkpad>
2014-04-28 22:24:28 +0200

    toolbar in administrator changed - note: no active menu highlighted

---
+++ backend/helpers/db8usergroups-old.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @package     db8usergroups
+ * @author	Peter Martin
+ * @copyright   Copyright (C) 2014 Peter Martin / db8.nl
+ * @license     GNU General Public License version 2 or later
+ */
+defined('_JEXEC') or die();
+
+/**
+ * Helper to display db8 locate component submenus in com_categories
+ */
+abstract class Db8usergroupsHelper {
+
+    /**
+     * Configure the Linkbar.
+     */
+    public static function addSubmenu($submenu) {
+        JSubMenuHelper::addEntry(JText::_('COM_DB8USERGROUPS_TITLE_ITEMS'), 
+                'index.php?option=com_db8usergroups', 
+                $submenu == 'locations');
+        JSubMenuHelper::addEntry(JText::_('COM_DB8USERGROUPS_SUBMENU_CATEGORIES'), 
+                'index.php?option=com_categories&view=categories&extension=com_db8usergroups', 
+                $submenu == 'categories');
+
+    }
+
+}
--- backend/helpers/db8usergroups.php
+++ backend/helpers/db8usergroups.php
@@ -1,29 +1,36 @@
 <?php
 
 /**
- * @package     db8usergroups
- * @author	Peter Martin
- * @copyright   Copyright (C) 2014 Peter Martin / db8.nl
- * @license     GNU General Public License version 2 or later
+  @package     db8usergroups
+  @author	Peter Martin
+  @copyright   Copyright (C) 2014 Peter Martin / db8.nl
+  @license     GNU General Public License version 2 or later
  */
 defined('_JEXEC') or die();
 
 /**
- * Helper to display db8 locate component submenus in com_categories
+  Helper to display db8 locate component submenus in com_categories
  */
 abstract class Db8usergroupsHelper {
 
     /**
-     * Configure the Linkbar.
+      Configure the Linkbar.
      */
-    public static function addSubmenu($submenu) {
-        JSubMenuHelper::addEntry(JText::_('COM_DB8USERGROUPS_TITLE_ITEMS'), 
-                'index.php?option=com_db8usergroups', 
-                $submenu == 'locations');
-        JSubMenuHelper::addEntry(JText::_('COM_DB8USERGROUPS_SUBMENU_CATEGORIES'), 
-                'index.php?option=com_categories&view=categories&extension=com_db8usergroups', 
-                $submenu == 'categories');
+    public static function addSubmenu($vName) {
+        // Load FOF
+        include_once JPATH_LIBRARIES . '/fof/include.php';
 
+        if (!defined('FOF_INCLUDED')) {
+            JError::raiseError('500', 'FOF is not installed');
+        }
+
+        if (version_compare(JVERSION, '3.0', 'ge')) {
+            $strapper = new FOFRenderJoomla3;
+        } else {
+            $strapper = new FOFRenderJoomla;
+        }
+
+        $strapper->renderCategoryLinkbar('com_db8usergroups');
     }
 
 }
